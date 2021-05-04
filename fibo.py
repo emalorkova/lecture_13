7ed781ad2c50de035fca003acecebb351e88f505
emalorkova <xlorko00@vutbr.cz>
2021-05-04 17:14:58 +0200

    Fibo rekurz

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return (recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2))
+
 
 def main():
-    pass
+    prvok = int(input("Zadaj prvok: "))
+    postupnost = [recursive_nth_fibo(n) for n in range(prvok+1)]
+    print(recursive_nth_fibo(prvok))
+    print(postupnost)
 
 
 if __name__ == '__main__':
